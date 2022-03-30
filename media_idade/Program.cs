5d4bcf941f0b5f75c1869612850a1cb981edc39c
Usuario <Usuario@CPD02-M05>
2022-03-30 10:52:56 -0300

    Adicionar arquivos de projeto.

---
+++ media_idade/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace media_idade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a idade do primeiro aluno: ");
+
+            int aluno1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a idade do segundo aluno: ");
+
+            int aluno2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a idade do terceiro aluno: ");
+
+            int aluno3 = Convert.ToInt32(Console.ReadLine());
+
+            int media_idade = (aluno1 + aluno2 + aluno3) / 3;
+
+            Console.Write("A média das idades é : " + media_idade);
+
+            Console.ReadKey();
+
+        }
+    }
+}
